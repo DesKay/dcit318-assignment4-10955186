9980ecb2abc25ab9a8f01fcaaff9aa50a1e9eb6b
DesKay <dokuffour@st.ug.edu.gh>
2024-07-08 12:40:40 +0000

    Address Book

---
+++ Address Book/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Address_Book
+{
+    public partial class Form1 : Form
+    {
+        private List<Person> addressBook = new List<Person>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Person person = new Person
+            {
+                Name = txtName.Text,
+                Email = txtEmail.Text,
+                Phone = txtPhone.Text
+            };
+            addressBook.Add(person);
+            MessageBox.Show("Entry saved successfully!");
+            txtName.Clear();
+            txtEmail.Clear();
+            txtPhone.Clear();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
